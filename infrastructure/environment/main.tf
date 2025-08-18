10153c2bb0393dce445c215ec21c769c076853f6
psalm-designer <parkerhorn@protonmail.com>
2025-08-17 21:07:08 -0400

    Static web app

--- infrastructure/environment/main.tf
+++ infrastructure/environment/main.tf
@@ -139,22 +139,26 @@ resource "azurerm_key_vault_access_policy" "web_app_access_to_kv" {
   ]
 }
 
-resource "azurerm_linux_web_app" "wedding_client" {
+resource "azurerm_static_web_app" "wedding_client" {
   name                = "wedding-client-${local.environment}"
   resource_group_name = azurerm_resource_group.wedding_api_env_rg.name
-  location            = azurerm_resource_group.wedding_api_env_rg.location
-  service_plan_id     = azurerm_service_plan.wedding_api_asp.id
+  location            = local.location
+  sku_tier            = "Free"
+  sku_size            = "Free"
   tags                = local.tags
 
-  site_config {
-    application_stack {
-      node_version = "20-lts"
-    }
-    always_on = false
-  }
-
   app_settings = {
-    "API_BASE_URL" = "https://${azurerm_linux_web_app.wedding_api.default_hostname}"
-    "WEBSITE_RUN_FROM_PACKAGE" = "1"
+    "WEDDING_API_BASE_URL" = "https://${azurerm_linux_web_app.wedding_api.default_hostname}"
   }
+}
+
+resource "azurerm_key_vault_secret" "static_web_app_deployment_token" {
+  name         = "StaticWebAppDeploymentToken${local.environment}"
+  value        = azurerm_static_web_app.wedding_client.api_key
+  key_vault_id = data.azurerm_key_vault.wedding_api_kv.id
+  tags         = local.tags
+
+  depends_on = [
+    azurerm_static_web_app.wedding_client
+  ]
 } 
